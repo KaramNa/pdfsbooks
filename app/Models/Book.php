4c9e34afdbc8c1bfc9b2596ad8e1861cc6226f20
karam <karam.nassar@hotmail.com>
2022-01-01 20:49:27 +0200

    remove search in desc

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -36,7 +36,7 @@ class Book extends Model
         $query->where(
             fn ($query) =>
             $query->where("title", "like", "%" . $search . "%")
-                ->orWhere("description", "like", "%" . $search . "%")
+                // ->orWhere("description", "like", "%" . $search . "%")
                 ->orWhere("author", "like", "%" . $search . "%")
         ),
             $this->SearchResults($query, $search)
