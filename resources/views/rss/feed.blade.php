57e6b1110a0a0ae8a134efa5f65431274734c1eb
karam <karam.nassar@hotmail.com>
2022-01-02 15:09:57 +0200

    fix rss poster

--- resources/views/rss/feed.blade.php
+++ resources/views/rss/feed.blade.php
@@ -1,22 +1,39 @@
 <?=
-'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
-?>
+'<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
 <rss version="2.0">
     <channel>
-        <title><![CDATA[ Pdfsbooks.com ]]></title>
-        <link><![CDATA[ https://pdfsbooks.com/feed ]]></link>
-        <description><![CDATA[ Online library for download pdf books for free ]]></description>
+        <title>
+            <![CDATA[ Pdfsbooks.com ]]>
+        </title>
+        <link>
+        <![CDATA[ https://pdfsbooks.com/feed ]]>
+        </link>
+        <description>
+            <![CDATA[ Online library for download pdf books for free ]]>
+        </description>
         <language>en</language>
         <pubDate>{{ now()->toDayDateTimeString('Asia/Damasucs') }}</pubDate>
 
-        @foreach($books as $book)
+        @foreach ($books as $book)
             <item>
-                <title><![CDATA[{{ $book->title }}]]></title>
-                <link>{{ "https://pdfsbooks.com/book/" . $book->slug }}</link>
-                <description><![CDATA[{!! $book->description !!}]]></description>
-                <poster>{{ "https://pdfsbooks.com" . $book->poster }}</poster>
+                <title>
+                    <![CDATA[{{ $book->title }}]]>
+                </title>
+                <link>{{ 'https://pdfsbooks.com/book/' . $book->slug }}</link>
+                <description>
+                    <![CDATA[{!! $book->description !!}]]>
+                </description>
+
+                <image>
+                    <url>{{ 'https://pdfsbooks.com' . $book->poster }}</url>
+                    <title>{{ $book->title }}]</title>
+                    <link>https://www.pdfsbooks.com</link>
+                </image>
+                <poster>{{ 'https://pdfsbooks.com' . $book->poster }}</poster>
                 <category>{{ $book->category }}</category>
-                <author><![CDATA[{{ $book->author  }}]]></author>
+                <author>
+                    <![CDATA[{{ $book->author }}]]>
+                </author>
             </item>
         @endforeach
     </channel>
