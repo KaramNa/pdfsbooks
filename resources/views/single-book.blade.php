44f7764495869dc60f2cf43db1c818e35045e674
karam <karam.nassar@hotmail.com>
2022-01-23 13:55:33 +0200

    add schema structure

--- resources/views/single-book.blade.php
+++ resources/views/single-book.blade.php
@@ -365,7 +365,7 @@
                 <div itemscope itemtype="https://schema.org/Book" itemid="https://pdfsbooks.com/book/{{ $book->slug }}">
                     <img itemprop="image" src="{{ asset($book->poster) }}" alt="cover art: {{ $book->title }}" />
                     <h1><span itemprop="name">{{ $book->title }}</span></h1>
-                    <div>Author: <span itemprop="author" itemscope>{{ substr($book->author, 3) }}</span></div>
+                    <div>Author: <span itemprop="author">{{ substr($book->author, 3) }}</span></div>
                     <div>Language:
                         <meta itemprop="inLanguage" content="en" />English
                     </div>
