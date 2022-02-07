4e2fdeaa22d8a9175776986bcaebaff8a48e7e13
karam <karam.nassar@hotmail.com>
2022-02-07 02:37:06 +0200

    feed ad

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,7 +56,7 @@
                 <div class="grid">
                     @foreach ($books as $book)
                         @if (in_array($loop->index, [1, 6, 9, 12, 15, 17]))
-                            <div class="ads mb-4">
+                            <div class="ads d-none mb-4">
                                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2052289648779673"
                                                                 crossorigin="anonymous"></script>
                                 <ins class="adsbygoogle" style="display:block" data-ad-format="fluid"
