b4514ac6368dac4c77a7e66415651831a2cc4eaa
karam <karam.nassar@hotmail.com>
2022-02-19 18:10:10 +0200

    fix free books search

--- resources/views/Components/kindle.blade.php
+++ resources/views/Components/kindle.blade.php
@@ -4,5 +4,5 @@
             Unlimited Reading. Unlimited Listening. Any Device. Take this Chance Now
         </p>
         <a href="https://www.amazon.com/kindle-dbs/hz/subscribe/ku?linkCode=pf4&fbclid=IwAR2IGBDArwAfCRcOEMkkOSP5qpHOO94qSoHkDXbr9oX_xmptIU6O1coSNJM&linkId=2ebf7923345196b8b8d298df34168234&_encoding=UTF8&ref_=assoc_tag_ph_1454291293420&tag=pdfsbooks-20&creative=9325&camp=1789&shoppingPortalEnabled=true"
-            title="Amazon Kindle Unlimited" class="btn btn-warning">Sing in</a>
+            title="Amazon Kindle Unlimited" class="btn btn-warning" target="_blank">Sing in</a>
     </div>
