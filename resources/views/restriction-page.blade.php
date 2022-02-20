54cd9ae707ecaf49531bfa98b97515cf18cf7114
karam <karam.nassar@hotmail.com>
2022-02-20 19:00:59 +0200

    d

--- resources/views/restriction-page.blade.php
+++ resources/views/restriction-page.blade.php
@@ -4,13 +4,6 @@
     <meta name="robots" content="noindex, nofollow">
   </head>
   <body style="background-color:#e4e4e4">
-    <div style="position:absolute; top:-200; bottom:0; left:0; right:0; margin:auto; height:200px; width: 750px">
-      <div style="text-align:center">
-        <h1 style="font-size:5rem; color:#800000 ">PDFSBOOKS.COM</h1>
-      </div>
-      <h1 style="text-align:center; font-family:Helvetica, Arial, sans-serif; font-weight:100; color: #333">
-        We are sorry, but due to some restrictions, you are not allowed to access the website.
-      </h1>
-    </div>
+    
   </body>
 </html>
