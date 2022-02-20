36e68a9a120c01d4d739dcccbeea05086661de3e
karam <karam.nassar@hotmail.com>
2022-02-20 19:02:38 +0200

    red

--- resources/views/restriction-page.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <title>Coursera - OFAC Restrictions</title>
-    <meta name="robots" content="noindex, nofollow">
-  </head>
-  <body style="background-color:#e4e4e4">
-    
-  </body>
-</html>
