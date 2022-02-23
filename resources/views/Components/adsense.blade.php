93d097a0703b9a68d1ac18b5a4ce963292c4d19c
karam <karam.nassar@hotmail.com>
2022-02-23 16:46:46 +0200

    add related books to get the link page

--- resources/views/Components/adsense.blade.php
+++ resources/views/Components/adsense.blade.php
@@ -1,4 +1,4 @@
-    <div class="my-3 ads d-none" onclick="hideAds()">
+    <div class="my-50 ads d-none" onclick="hideAds()">
         <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article"
         data-ad-format="fluid" data-ad-client="ca-pub-2052289648779673" data-ad-slot="3036230486"></ins>
     <script>
