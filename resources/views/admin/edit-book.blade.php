4a5adec05e78002adf66b86310e4ae4b55029014
karam <karam.nassar@hotmail.com>
2022-04-08 14:10:25 +0300

    fix tag in edit

--- resources/views/admin/edit-book.blade.php
+++ resources/views/admin/edit-book.blade.php
@@ -249,5 +249,8 @@
                 }
             });
         });
+         $(document).on('select2:open', () => {
+                document.querySelector('.select2-search__field').focus();
+            });
     </script>
 @stop
