0b558de7a5275011097cacd798886bb85d98e5e2
karam <karam.nassar@hotmail.com>
2022-06-30 15:14:28 +0300

    update stat counter

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,8 +7,8 @@
                     <a href="https://www.facebook.com/groups/594645551607306" class="facebook" target="_blank"
                         title="Facebook group link">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
-                            class="svg-inline--fa fa-facebook-f fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 320 512" width="25">
+                            class="svg-inline--fa fa-facebook-f fa-w-10" role="img"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="25">
                             <path fill="currentColor"
                                 d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
                             </path>
@@ -16,8 +16,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="https://t.me/e_pdfsbooks" class="telegram" target="_blank"
-                        title="Telegram channel link">
+                    <a href="https://t.me/e_pdfsbooks" class="telegram" target="_blank" title="Telegram channel link">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="telegram-plane"
                             class="svg-inline--fa fa-telegram-plane fa-w-14" role="img"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="30">
@@ -28,8 +27,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/pdfsbooks_" class="twitter" target="_blank"
-                        title="Twitter link">
+                    <a href="https://twitter.com/pdfsbooks_" class="twitter" target="_blank" title="Twitter link">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
                             class="svg-inline--fa fa-twitter fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512" width="30">
@@ -53,8 +51,8 @@
                     <a href="https://www.pinterest.com/freepdfsbooks1" class="pinterest" target="_blank"
                         title="pinterest link">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="pinterest-p"
-                            class="svg-inline--fa fa-pinterest-p fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 384 512" width="25">
+                            class="svg-inline--fa fa-pinterest-p fa-w-12" role="img"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="25">
                             <path fill="currentColor"
                                 d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z">
                             </path>
@@ -209,17 +207,18 @@
     }
 </script>
 
-<!-- Default Statcounter code for pdf https://pdfsbooks.com -->
+<!-- Default Statcounter code for pdfsbooks https://pdfsbooks.com -->
 <script type="text/javascript">
-    var sc_project = 12758071;
+    var sc_project = 12770038;
     var sc_invisible = 1;
-    var sc_security = "0a401d87";
+    var sc_security = "36fccb49";
 </script>
 <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
 <noscript>
-    <div class="statcounter"><a title="website statistics" href="https://statcounter.com/" target="_blank"><img
-                class="statcounter" src="https://c.statcounter.com/12758071/0/0a401d87/1/" alt="website statistics"
-                referrerPolicy="no-referrer-when-downgrade"></a></div>
+    <div class="statcounter"><a title="Web Analytics Made Easy -
+Statcounter" href="https://statcounter.com/"
+            target="_blank"><img class="statcounter" src="https://c.statcounter.com/12770038/0/36fccb49/1/"
+                alt="Web Analytics Made Easy - Statcounter" referrerPolicy="no-referrer-when-downgrade"></a></div>
 </noscript>
 <!-- End of Statcounter Code -->
 
